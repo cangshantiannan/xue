998293b96aca6058ad053ee8505a26a77b5e7fce
wyl <959814898@qq.com>
2019-12-15 22:10:38 +0800

    建立项目

---
+++ src/main/java/com/wyl/xue/XueApplication.java
@@ -0,0 +1,13 @@
+package com.wyl.xue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyl/xue/XueApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wyl.xue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
