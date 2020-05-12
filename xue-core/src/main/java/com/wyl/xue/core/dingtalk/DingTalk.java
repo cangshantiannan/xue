28f6a671ce9ec2e6257c203ec5b0125556dd531d
wyl <959814898@qq.com>
2020-05-12 15:32:30 +0800

    增加钉钉消息测试

--- xue-core/src/main/java/com/wyl/xue/core/dingtalk/DingTalk.java
+++ xue-core/src/main/java/com/wyl/xue/core/dingtalk/DingTalk.java
@@ -1,7 +1,3 @@
-/**
- * @Author wangyl
- * @E-mail wangyl0629@foxmail.com
- **/
 package com.wyl.xue.core.dingtalk;
 
 import com.dingtalk.api.DefaultDingTalkClient;
@@ -32,7 +28,16 @@ public class DingTalk {
         request = new OapiRobotSendRequest();
     }
 
-    public void sentText(@NotNull final String msg, final List<String> phones) throws ApiException {
+    /**
+     * @Description 发送文字消息
+     * @param msg 文字消息
+     * @param phones 需要AT的人 null 为所有人
+     * @return void
+     * @Date 2020/5/12 3:25 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
+    public void sendText(@NotNull final String msg, final List<String> phones) throws ApiException {
         request.setAt(makeAt(phones));
         OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
         text.setContent(msg);
@@ -41,6 +46,15 @@ public class DingTalk {
         sendToDingTalk();
     }
 
+    /**
+     * @Description
+     * @param link
+     * @param phones
+     * @return void
+     * @Date 2020/5/12 3:26 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
     public void sendLink(OapiRobotSendRequest.Link link, final List<String> phones) throws ApiException {
         request.setAt(makeAt(phones));
         request.setMsgtype("link");
@@ -48,7 +62,15 @@ public class DingTalk {
         sendToDingTalk();
     }
 
-
+    /**
+     * @Description
+     * @param markdown
+     * @param phones
+     * @return void
+     * @Date 2020/5/12 3:26 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
     public void sendMarkdown(OapiRobotSendRequest.Markdown markdown, final List<String> phones) throws ApiException {
         request.setAt(makeAt(phones));
         request.setMsgtype("markdown");
@@ -57,6 +79,15 @@ public class DingTalk {
     }
 
 
+    /**
+     * @Description
+     * @param actioncard
+     * @param phones
+     * @return void
+     * @Date 2020/5/12 3:26 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
     public void sendActionCard(OapiRobotSendRequest.Actioncard actioncard, final List<String> phones) throws ApiException {
         request.setAt(makeAt(phones));
         request.setMsgtype("actionCard");
@@ -65,12 +96,29 @@ public class DingTalk {
     }
 
 
+    /**
+     * @Description
+     * @param feedcard
+     * @param phones
+     * @return void
+     * @Date 2020/5/12 3:26 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
     public void sendFeedCard(OapiRobotSendRequest.Feedcard feedcard, final List<String> phones) throws ApiException {
         request.setAt(makeAt(phones));
         request.setFeedCard(feedcard);
         sendToDingTalk();
     }
 
+    /**
+     * @Description 组装At人 null为所有人
+     * @param phones
+     * @return com.dingtalk.api.request.OapiRobotSendRequest.At
+     * @Date 2020/5/12 3:26 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
     private OapiRobotSendRequest.At makeAt(final List<String> phones) {
         OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
         if (Objects.isNull(phones) || phones.isEmpty()) {
@@ -82,11 +130,20 @@ public class DingTalk {
         return at;
     }
 
+    /**
+     * @Description 给钉钉发送消息
+     * @param
+     * @return void
+     * @Date 2020/5/12 3:26 下午
+     * @Author wangyl
+     * @Version V1.0
+     */
     private void sendToDingTalk() throws ApiException {
         OapiRobotSendResponse response = client.execute(request);
         if (log.isDebugEnabled()) {
             log.debug("发送钉钉消息返回信息为[{}]", response);
         }
-        System.out.println(response.getErrmsg());
+        //TODO 需要增加异常处理
+        log.error(response.getErrmsg());
     }
 }
